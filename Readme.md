8112e84c552ce702e28b1c90ad5424cf9ff15a6c
Juan Sebastian Castillo Amaya <jcastilloamaya@outlook.com>
2025-06-19 11:19:26 -0500

    Tercer cambio

--- Readme.md
+++ Readme.md
@@ -2,4 +2,3 @@
 
 # second commit
 
-# second 
--- Test1.py
+++ Test1.py
@@ -2,4 +2,6 @@ print("Hola Git")
 
 print("Hola Git segundo commit")
 
-print("Hola Git segundo commit")
+print("Hola Git cambio trucho")
+
+print("Cambio")
